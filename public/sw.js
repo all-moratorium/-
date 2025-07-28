5487c65e8b9fac070fb542d7fd6ccfd92e59a48c
all-moratorium <ramunesheiku@gmail.com>
2025-07-28 11:04:28 +0900

    noasfd

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+// Simple service worker for PWA
+const CACHE_NAME = 'led-neon-sign-v1';
+const urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
