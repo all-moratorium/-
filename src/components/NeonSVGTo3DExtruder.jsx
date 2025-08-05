fb9f2c867cc8fa71d469bd0eb57b13ffa5de9076
all-moratorium <ramunesheiku@gmail.com>
2025-08-05 18:24:21 +0900

    aaa

--- src/components/NeonSVGTo3DExtruder.jsx
+++ src/components/NeonSVGTo3DExtruder.jsx
@@ -1888,7 +1888,7 @@ const NeonSVGTo3DExtruder = forwardRef(({ neonSvgData, backgroundColor = '#24242
             <span className="neon3d-dimension-value">{calculatedModelData?.isGenerated === true ? `${(calculatedModelData.tubeLength8mm / 10).toFixed(1)}cm` : 'N/A'}</span>
           </div>
           <div className="neon3d-dimension-item">
-            <span className="neon3d-dimension-label">{window.innerWidth <= 768 ? 'OFF時の\nチューブカラー' : 'OFF時のチューブカラー'}</span>
+            <span className="neon3d-dimension-label">OFF時の　<br />チューブカラー</span>
             <span className="neon3d-dimension-value neon3d-small-text">{calculatedModelData?.isGenerated === true ? (offTubeColor === 'white' ? 'ホワイト' : '発光色マッチング') : 'N/A'}</span>
           </div>
           <div className="neon3d-dimension-item">
