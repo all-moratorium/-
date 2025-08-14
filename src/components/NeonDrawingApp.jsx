27f848c80f3fc06aee38b668fa7ed21b937e72f2
all-moratorium <ramunesheiku@gmail.com>
2025-08-15 04:34:16 +0900

    kinouba-nande

--- src/components/NeonDrawingApp.jsx
+++ src/components/NeonDrawingApp.jsx
@@ -3634,13 +3634,20 @@ const NeonDrawingApp = ({ initialState, onStateChange, sharedFileData, onSharedF
                             onClick={startNewPath}
                             disabled={isNewPathDisabled || isModifyingPoints || isPathDeleteMode || isPointDeleteMode}
                         >
-                            新しい<br/>パス
+                            <svg width="23" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                                <path d="M12 5v14M5 12h14"/>
+                            </svg>
                         </button>
                         <button
                             className={`mobile-function-btn modify-points ${isModifyingPoints ? 'button-active' : ''}`}
                             onClick={() => setIsModifyingPoints(!isModifyingPoints)}
                         >
-                            点<br/>修正
+                            <svg width="24" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                                <path d="M18 11V6a2 2 0 0 0-2-2v0a2 2 0 0 0-2 2v0"></path>
+                                <path d="M14 10V4a2 2 0 0 0-2-2v0a2 2 0 0 0-2 2v2"></path>
+                                <path d="M10 10.5V6a2 2 0 0 0-2-2v0a2 2 0 0 0-2 2v8"></path>
+                                <path d="M18 8a2 2 0 1 1 4 0v6a8 8 0 0 1-8 8h-2c-2.8 0-4.5-.86-5.99-2.34l-3.6-3.6a2 2 0 0 1 2.83-2.82L8 16h4"></path>
+                            </svg>
                         </button>
                         <button
                             className={`mobile-function-btn undo ${historyIndex <= 0 ? 'button-disabled' : ''}`}
