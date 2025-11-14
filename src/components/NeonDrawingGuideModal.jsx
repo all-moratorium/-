43ab2c885d32acc4b3cf345315b1b7c9dabe52ec
all-moratorium <ramunesheiku@gmail.com>
2025-11-15 03:03:05 +0900

    asdf:

--- src/components/NeonDrawingGuideModal.jsx
+++ src/components/NeonDrawingGuideModal.jsx
@@ -345,7 +345,7 @@ const NeonDrawingGuideModal = ({ isOpen, onClose }) => {
                     </div>
                     <video
                       ref={videoRef}
-                      src="/ネオン下絵　ガイドモーダル/ネオンガイド1.mp4"
+                      src="/ネオン下絵　ガイドモーダル/ネオン下絵ガイド1(2).webm"
                       style={{ width: '100%', height: '100%', objectFit: 'cover' }}
                       autoPlay
                       loop
